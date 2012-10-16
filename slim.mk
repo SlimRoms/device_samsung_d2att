e53436a87b61b5768e8d34eb30bfef4fdf94fa30
gmillz <griffinn.millender@gmail.com>
2012-10-16 15:35:44 -0500

    Update slim.mk

--- slim.mk
+++ slim.mk
@@ -15,24 +15,6 @@ $(call inherit-product, vendor/slim/config/common_sgs.mk)
 PRODUCT_COPY_FILES +=  \
     vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
-#copy kernel and modules
-PRODUCT_COPY_FILES += \
-	device/samsung/d2-common/kernel/boot.img:system/slimkernel/boot.img \
-	device/samsung/d2-common/kernel/kernel:kernel \
-	device/samsung/d2-common/kernel/system/lib/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-	device/samsung/d2-common/kernel/system/lib/modules/qce40.ko:system/lib/modules/qce40.ko \
-	device/samsung/d2-common/kernel/system/lib/modules/spidev.ko:system/lib/modules/spidev.ko \
-	device/samsung/d2-common/kernel/system/lib/modules/qcrypto.ko:system/lib/modules/qcrypto.ko \
-	device/samsung/d2-common/kernel/system/lib/modules/qcedev.ko:system/lib/modules/qcedev.ko \
-	device/samsung/d2-common/kernel/system/lib/modules/vpnclient.ko:system/lib/modules/vpnclient.ko \
-	device/samsung/d2-common/kernel/system/lib/modules/gspca_main.ko:system/lib/modules/gpsca_main.ko \
-	device/samsung/d2-common/kernel/system/lib/modules/btlock.ko:system/lib/modules/btlock.ko \
-	device/samsung/d2-common/kernel/system/lib/modules/dhd.ko:system/lib/modules/dhd.ko \
-	device/samsung/d2-common/kernel/system/lib/modules/evbug.ko:system/lib/modules/evbug.ko \
-	device/samsung/d2-common/kernel/system/bin/thermald:system/bin/thermald \
-	device/samsung/d2-common/kernel/system/xbin/ffc:system/xbin/ffc \
-	device/samsung/d2-common/kernel/system/xbin/stocktherm:system/xbin/stocktherm
-
 #copy 00check
 PRODUCT_COPY_FILES += \
 	vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
